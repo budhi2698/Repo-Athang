64a29b8cc07b33eb55be9113c89cecc9d6f3dacc
budhi2698 <budhimangurung926@gmail.com>
2023-04-24 10:11:16 +0600

    new

--- training/resources/views/home.blade.php
+++ training/resources/views/home.blade.php
@@ -22,13 +22,14 @@
         <?php 
             include('nav.php') 
         ?>
-
+    
     <!----------------------------------------------image carousel-------------------------------------------------------->
+    
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="1000">
               <img src="ap.jpg" class="d-block w-100" alt="...">
-            </div>
+            </div> 
             <div class="carousel-item" data-bs-interval="2000">
               <img src="erp.jpg" class="d-block w-100" alt="...">
             </div>
