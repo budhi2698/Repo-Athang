0acc35eda81385452a065681ecbd5b7ec8c603cb
yas2021s <yaezerl@gmail.com>
2023-04-24 10:56:34 +0600

    changes

--- training/resources/views/home.blade.php
+++ training/resources/views/home.blade.php
@@ -46,16 +46,7 @@
             <span class="visually-hidden">Next</span>
         </button>
 </div>
-            
-      
-        <div class="card" style="width: 18rem; margin-left:80px;" >
-        <img src="ap.jpg" class="card-img-top" alt="..." height="150px;">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
-                Some quick example text to build on the card title and make up the bulk of the card's content
-                Some quick example text to build on the card title and make up the bulk of the card's content</p>
-            </div>
-        </div>
+
         <div class="card" style="width: 18rem; margin-left:80px;" >
         <img src="ap.jpg" class="card-img-top" alt="..." height="150px;">
             <div class="card-body">
