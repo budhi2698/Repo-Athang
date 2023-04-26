825115cf525856d344fafe12ce170139c587edd1
budhi2698 <budhimangurung926@gmail.com>
2023-04-26 16:39:49 +0600

    announce

--- training/resources/views/announcement.php
+++ training/resources/views/announcement.php
@@ -19,24 +19,45 @@
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+   
+
 
     </head>
     <body class="antialiased">
  
     <?php 
-            include('nav.php') 
-        ?>
+        include('nav.php') 
+     ?>
 
-    <section id="courses">
-        <h2>Our Courses</h2>
-        <ul>
-            <li>Web Development</li>
-            <li>Graphic Design</li>
-            <li>Networking</li>
-            <li>Project Management</li>
-            <li>ECCD</li>
-        </ul>
-    </section>
+    <div class="announcement">
+        <h3>Annoucement/News</h3>
+    </div>
+    <div class="anncontainer">
+        <div class="content bg-register">
+            <h1>Diploma 2nd Batch Registration</h1>
+            <img src="image1.jpg" alt="Image 1"> 
+            <p>Athang Training Academy is pleased to announce the 2nd batch training on ICT and 
+                Online Freelancing Program for 20 slots for a duration of 6 months supported by 
+                The Human Resource and Skills Development Division (HRSDD),Department of Employment and 
+                Human Resources (DoEHR), Registration ends by 23rd August 2021.
+            </p>
+        </div>
+        <div class="content2 bg-blue">
+            <h1>Diploma 2nd Batch Registration</h1>
+            <img src="image2.jpg" alt="Image 2"> 
+            <p>Our program offers hands-on training and industry-relevant courses.</p>
+        </div>
+        <div class="content3 bg-red">
+            <h1>Diploma 2nd Batch Registration</h1>
+            <img src="image2.jpg" alt="Image 3"> 
+            <p>Our program offers hands-on training and industry-relevant courses.</p>
+        </div>
+        <div class="content4 bg-org">
+            <h1>Diploma 2nd Batch Registration</h1> 
+            <img src="image4.jpg" alt="Image 4">
+            <p>Join us now and become part of a growing community of skilled professionals.</p>
+        </div>
+    </div>
 
     <?php 
         include('footer.php')
