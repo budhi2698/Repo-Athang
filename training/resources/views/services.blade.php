570a9bcb56b8771b0e8b7d1395113b9cc2322fff
budhi2698 <budhimangurung926@gmail.com>
2023-04-28 09:51:25 +0600

    services

--- training/resources/views/services.blade.php
+++ training/resources/views/services.blade.php
@@ -16,26 +16,62 @@
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-       
+
     </head>
 <body>
         <?php 
             include('nav.php') 
         ?>
-        <section id="courses">
-            <h2>Our Courses</h2>
-            <ul>
-            <li>Web Development</li>
-            <li>Graphic Design</li>
-            <li>Networking</li>
-            <li>Project Management</li>
-            <li>ECCD</li>
-            </ul>
-            <div class="click">
-            <a href="register">More...</a>
-            </div> 
+        <section id=servics>
+            <h2>Our Services</h2>
+            <p>Athang plays a vital lead role in the ever changing ICT scenario in Bhutan. With our team of professional expertise 
+                abreast and adept with the ICT trend, Athang offers its software development expertise in designing, developing and 
+                re-engineering the ICT requirements of our clients. Over a decade, our software engineers have executed several 
+                interesting projects starting from a simple website to many complex systems. And the recent achievement would be 
+                the development and successful rollout of an Enterprise Resource Planning system.
+            </p>
         </section>
 
+        <section id="courses">                                  
+            <h2>Courses</h2>
+            <div class="servicecontainer">
+                <div class="colum">
+                    <h1>Web Development</h1>
+                    <img src="webdev.jpg" alt="Web development" style="border-radius:10px">
+                    <p>Athang Training Academy is pleased to announce the 2nd batch training on ICT and Online Freelancing Program for 20 slots for a duration of 6 months supported by</p>
+                </div>
+                <div class="colum">
+                    <h1>Graphic Design</h1>
+                    <img src="graphic.jpg" alt="Graphic design" style="border-radius:10px">
+                    <p>Athang Training Academy is pleased to announce the 2nd batch training on ICT and Online Freelancing Program for 20 slots for a duration of 6 months supported by</p>
+                </div>
+                <div class="colum">
+                    <h1>Networking</h1>
+                    <img src="network.jpg" alt="Networking"; style="border-radius:10px">
+                    <p>Athang Training Academy is pleased to announce the 2nd batch training on ICT and Online Freelancing Program for 20 slots for a duration of 6 months supported by</p>
+                </div>
+                <div class="colum">
+                    <h1>Project Management</h1>
+                    <img src="project.png" alt="Image 1" style="border-radius:10px">
+                    <p>Athang Training Academy is pleased to announce the 2nd batch training on ICT and Online Freelancing Program for 20 slots for a duration of 6 months supported by</p>
+                </div>
+                <div class="colum">
+                    <h1>ECCD</h1>
+                    <img src="child.jpg" alt="Eccd" style="border-radius:10px">
+                    <p>Athang Training Academy is pleased to announce the 2nd batch training on ICT and Online Freelancing Program for 20 slots for a duration of 6 months supported by</p>
+                </div>
+                <div class="colum">
+                    <h1>General Training</h1>
+                    <img src="gene.jpg" alt="Image 1" style="border-radius:10px">
+                    <p>Athang Training Academy is pleased to announce the 2nd batch training on ICT and Online Freelancing Program for 20 slots for a duration of 6 months supported by</p>
+                </div>
+            </div>
+        </section>
+         <section id="regis">
+             <div class="click">
+               <a href="register">Register here..</a>
+            </div>  
+         </section>
     <?php 
             include('footer.php')    
     ?>
